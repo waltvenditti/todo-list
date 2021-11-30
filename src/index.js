297f006d7d3bc88ebe186925496a2aa6a51321c8
WaltVenditi <waltvenditti@gmail.com>
2021-11-30 16:57:42 -0500

    made some notes and sketched out some stuff in js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './style.css';
 //import {something} from 'date-fns';
 
+//create basic html elements and display them 
+
+//a task is an object with properties title, desc, due date, priority. Contains methods in the form of functions for modifying these properties? 
+
+//a project is an array of task objects. It may have methods associated with it
+
+//there should be an array of projects. 
+
+//startup:
+    // get array from storage
+    // if none, or if empty, create default project 
+    // otherwise, populate page with existing projects 
+
+//a factory function for making new tasks
+
+const todoActionFactory = (title, desc, dueDate, priority) => {
+    
+    return {title, desc, dueDate, priority}
+};
